4aee2fa492c8dcb1c9929307a44fe451bfc9611e
Defy <61808223+De-y@users.noreply.github.com>
2024-06-28 11:40:34 -0700

    whats going on

---
+++ src/components/platform/AccountComponent.tsx
@@ -0,0 +1,48 @@
+'use client';
+import '@/css/platform/account.css'
+import { Button, TextInput } from '@mantine/core'
+import { useRouter } from 'next/navigation';
+export default function AccountComponent() {
+    let router = useRouter()
+    function accessAccount(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+        // Gets form
+        let form = event.currentTarget as HTMLFormElement;
+        // Gets credentials
+        let [email, password] = [form.email.value, form.password.value]
+        // Might be complicated but basically we are submitting the email and password to the API so we can authenticate
+        fetch('/api/v1/account/access', {
+            'method': 'POST',
+            'body': JSON.stringify({
+                'email': email,
+                'password': password
+            })
+        }).then(d=>{/*Basically this just gets .json within promise of fetch so we can get the actual data*/d.json().then(data=>{
+            console.log(data)
+            if (data['HORIZ_STAT'] == "LOGGED_IN") {
+                router.push('/platform')
+            } else {
+                let status_bar = document.getElementById('oris-status') as HTMLElement
+                status_bar.innerHTML = 'Invalid credentials. Please try again.'
+            }
+        })})
+    }
+    return (
+        <>
+            <div className='platform'>
+                <div className='account-box'>
+                    <h1>Oris Account</h1>
+                    <p>You can login or create an Oris account through here.</p>
+                    <p id='oris-status'></p>
+                    {/* Submits to our function to authenticate */}
+                    <form className='access' onSubmit={accessAccount}>
+                        <TextInput className='input' variant='filled' label="Email:" type="email" name='email' placeholder='df@avnce.org' required />
+                        <TextInput className='input' variant='filled' label="Password:" type="password" name='password' placeholder='Password' required />
+                        <Button color='#4FC1DF' type='submit'>Access Account</Button>
+                    </form>
+                    <p>©{new Date().getFullYear()} Loom.</p>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,10 +2,22 @@ import { serial, text, pgTable, pgSchema } from "drizzle-orm/pg-core";
 
 export const orisSchema = pgSchema('oris_schema')
 
+export const role = orisSchema.enum('role', ['admin', 'user'])
+
 export const account = orisSchema.table('account', {
     id: serial('id').primaryKey(),
     email: text('email'),
     password: text('password'),
     saltA: text('saltA'),
     saltB: text('saltB')
+})
+
+export const accountInformation = orisSchema.table('accountinformation', {
+    id: serial('id').primaryKey(),
+    account_id: serial('id'),
+    image_url: text('image_url'),
+    first_name: text('first_name'),
+    last_name: text('last_name'),
+    organization: text('organization'),
+    user_role: role('user_role')
 })
